ab5ffbb0c2084d584851404899e9519be005081e
Konboi <ryosuke.yabuki@gmail.com>
2014-06-28 19:46:01 +0900

    Fix gopath

--- inits/50_go-mode.el
+++ inits/50_go-mode.el
@@ -1,12 +1,8 @@
-(require ;go-mode)
-(require 'go-autocomplete)
-(require 'go-flymake)
-
 (setenv "GOROOT" "/usr/local/go")
-(setenv "GOPATH" "~/workspace/go/bin")
+(setenv "GOPATH" "~/workspace/go")
 
 (add-to-list 'exec-path (expand-file-name "/usr/local/go/bin"))
-(add-to-list 'exec-path (expand-file-name "~/workspace/go/bin"))
+(add-to-list 'exec-path (expand-file-name "/home/vagrant//workspace/go/bin"))
 
 (eval-after-load "go-mode"
   '(progn
