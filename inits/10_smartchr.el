124a19619d05d9d6994f2bf825ffe9fbd79c12bc
Konboi <ryosuke.yabuki@gmail.com>
2014-08-12 00:38:28 +0900

    Fix smartchr config

--- inits/10_smartchr.el
+++ inits/10_smartchr.el
@@ -4,27 +4,29 @@
              (progn
                (local-set-key (kbd "<") (smartchr '("<" "<% `!!' %>" "<%= `!!' %>" "<")))
                )))
-(add-hook 'php-mode
+(add-hook 'php-mode-hook
           '(lambda ()
              (progn
                (local-set-key (kbd "<") (smartchr '("<?php `!!' ?>" "<?php" "<")))
                )))
 
-(add-hook 'cperl-mode
+(add-hook 'cperl-mode-hook
           '(lambda ()
              (progn
+               (local-set-key (kbd "F") (smartchr '("F" "$")))
+               (local-set-key (kbd "m") (smartchr '("m" "my")))
                (local-set-key (kbd "=") (smartchr '("=" "=>" "==")))
                (local-set-key (kbd "-") (smartchr '("-" "->" "--")))
                )))
-(add-hook 'ruby-mode
+(add-hook 'ruby-mode-hook
           '(lambda ()
              (progn
                (local-set-key (kbd "=") (smartchr '("=" "=>" "==")))
                )))
 
-(add-hook 'tx-mode
+(add-hook 'tx-mode-hook
           '(lambda ()
              (progn
-               (local-set-key (kbd "<") (smartchr '("<" "<:")))
+               (local-set-key (kbd "<") (smartchr '("<" "<:" "<: `!! :>")))
                (local-set-key (kbd ">") (smartchr '(">" ":>")))
                )))
