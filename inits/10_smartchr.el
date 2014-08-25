f41e42b0f403834cd0ce3de56ce33539ab341256
Konboi <ryosuke.yabuki@gmail.com>
2014-08-25 20:03:57 +0900

    修正

--- inits/10_smartchr.el
+++ inits/10_smartchr.el
@@ -15,13 +15,13 @@
              (progn
                (local-set-key (kbd "F") (smartchr '("F" "$")))
                (local-set-key (kbd "m") (smartchr '("m" "my ")))
-               (local-set-key (kbd "=") (smartchr '("=" "=>" "==")))
-               (local-set-key (kbd "-") (smartchr '("-" "->" "--")))
+               (local-set-key (kbd "=") (smartchr '("=" "=> " "== ")))
+               (local-set-key (kbd "-") (smartchr '("-" "->" "-- ")))
                )))
 (add-hook 'ruby-mode-hook
           '(lambda ()
              (progn
-               (local-set-key (kbd "=") (smartchr '("=" "=>" "==")))
+               (local-set-key (kbd "=") (smartchr '("=" "=> " "== ")))
                )))
 
 (add-hook 'tx-mode-hook
