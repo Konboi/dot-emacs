aff74fe82d11df42c50e6b65aafbe025eaa28928
Konboi <ryosuke.yabuki@gmail.com>
2014-10-22 08:45:52 +0900

    c-modeの設定追加

--- inits/50_c++-mode.el
+++ inits/50_c++-mode.el
@@ -1,3 +1,15 @@
+;; c-mode initialization
+;; C言語用モード起動時にやること
+(add-hook 'c-mode-hook
+          '(lambda ()
+             ;; indentation styles
+             ;; インデントスタイル
+             (c-set-style "stroustrup")
+             (setq indent-tabs-mode nil)     ; インデントは空白文字で行う（TABコードを空白に変換）
+             (c-set-offset 'innamespace 0)
+             (setq c-basic-offset 2)
+             (local-set-key (kbd "C-c C-c") 'quickrun-compile-only)))
+
 ;; C++ style
 (add-hook 'c++-mode-hook
           '(lambda()
@@ -5,4 +17,4 @@
              (setq indent-tabs-mode nil)     ; インデントは空白文字で行う（TABコードを空白に変換）
              (c-set-offset 'innamespace 0)   ; namespace {}の中はインデントしない
              (c-set-offset 'arglist-close 0) ; 関数の引数リストの閉じ括弧はインデントしない
-             )) 
+             ))
