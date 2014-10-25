01467f4c4e225c411122d74b788cfb89163d1d05
Konboi <ryosuke.yabuki@gmail.com>
2014-10-25 12:48:06 +0900

    localのkey map に割り当てる

--- inits/50_helm-rails.el
+++ inits/50_helm-rails.el
@@ -1,8 +1,9 @@
 (require 'helm-rails)
 
-(define-key global-map (kbd "C-c C-c") 'helm-rails-controllers)
-(define-key global-map (kbd "s-y") 'helm-rails-models)
-(define-key global-map (kbd "s-u") 'helm-rails-views)
-(define-key global-map (kbd "s-o") 'helm-rails-specs)
-(define-key global-map (kbd "s-r") 'helm-rails-all)
-
+(add-hook 'ruby-mode
+          '(lambda ()
+             (local-set-key (kbd "C-c C-c") 'helm-rails-controllers)
+             (local-set-key (kbd "s-y") 'helm-rails-models)
+             (local-set-key (kbd "s-u") 'helm-rails-views)
+             (local-set-key (kbd "s-o") 'helm-rails-specs)
+             (local-set-key (kbd "s-r") 'helm-rails-all)))
