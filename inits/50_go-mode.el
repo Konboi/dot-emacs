11e87b4e459187b4c4b11c3e24e52cf6d79e91ef
Konboi <ryosuke.yabuki@gmail.com>
2014-10-30 21:13:41 +0900

    コマンドを追加

--- inits/50_go-mode.el
+++ inits/50_go-mode.el
@@ -11,7 +11,10 @@
 
 (add-hook 'go-mode-hook
           (lambda ()
+            (local-set_ket (kbd "C-c C-c" 'go-remove-unused-imports)
+            (local-set-key (kbd "M-p") 'godoc)
             (local-set-key (kbd "M-]") 'godef-jump)))
+
 
 ;; http://d.hatena.ne.jp/syohex/
 (font-lock-add-keywords
