d580343389748a831c613299e0b10c69d18b5bba
Konboi <ryosuke.yabuki@gmail.com>
2014-11-04 23:02:10 +0900

    fixed typo

--- inits/50_go-mode.el
+++ inits/50_go-mode.el
@@ -11,9 +11,9 @@
 
 (add-hook 'go-mode-hook
           (lambda ()
-            (local-set_ket (kbd "C-c C-c" 'go-remove-unused-imports)
+            (local-set-key (kbd "C-c C-c") 'go-remove-unused-imports)
             (local-set-key (kbd "M-p") 'godoc)
-            (local-set-key (kbd "M-]") 'godef-jump))))
+            (local-set-key (kbd "M-]") 'godef-jump)))
 
 
 ;; http://d.hatena.ne.jp/syohex/
