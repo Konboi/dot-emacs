3ef53990dbc934a813797582913ad4571061218c
Konboi <ryosuke.yabuki@gmail.com>
2015-08-24 15:47:06 +0900

    なんか重いので削除する

--- inits/30_inf-ruby-mode.el
+++
@@ -1,3 +0,0 @@
-;; inf-ruby.el
-(autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
-(add-hook 'ruby-mode-hook 'inf-ruby-minor-mode)
--- inits/60_ruby-electric-mode.el
+++
@@ -1,3 +0,0 @@
-;; ruby-electric.el --- electric editing commands for ruby files
-(require 'ruby-electric)
-(add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
