cf465fa5a839672ef477972c32d2710c65c48d76
Konboi <ryosuke.yabuki@gmail.com>
2015-11-30 20:14:03 +0900

    更新

--- inits/50_org-mode.el
+++ inits/50_org-mode.el
@@ -1,4 +1,15 @@
+(require 'smartchr)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+
+;; capture templates
+(setq org-capture-templates
+      '(("t" "TODO" entry (file+headline (expand-file-name "~/.emacs.d/org/task.org") "INBOX")
+         "** TODO %?\n    %i\n    %a\n    %T")
+        ("m" "Memo" entry (file (expand-file-name "~/.emacs.d/org/memo.org"))
+                      "* %?\n    %i\n    %a\n    %T")))
+
+;; agenda
+(setq org-agenda-files (list (expand-file-name "~/.emacs.d/org")))
 
 ;; key-binding
 (global-set-key "\C-cl" `org-store-link)
@@ -8,7 +19,7 @@
 
 ;; TODO cycle
 (setq org-todo-keywords
-      '((sequence "TODO" "DOING" "FEEDBACK" "|" "DONE" "DELEGATED")))
+      '((sequence "TODO(t)" "DOING(d@)" "STOP(s@)" "|" "DONE(f!)")))
 
 (setq org-log-done 'time)
 (setq org-log-done 'note)
